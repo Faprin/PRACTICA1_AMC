dd2f74c6a725f1523f4b5bf4346b6d585ca65c74
Faprin <jmdominguezsegura@gmail.com>
2024-10-09 13:20:57 +0200

    archivo de Frame

--- src/main/java/App/Algoritmos.java
+++ src/main/java/App/Algoritmos.java
@@ -197,4 +197,5 @@ public class Algoritmos {
         return retorno;
     }
 
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.practica1_prueba {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
             
                             
     opens com.example.practica1_prueba to javafx.fxml;
